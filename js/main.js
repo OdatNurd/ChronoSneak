1af28c5299151e8c8608c4535a142dcd1bd38232
Terence Martin <poindexter@nurdz.com>
2015-10-20 14:04:52 -0700

    Slight refactor
    Since constants are used so much in main, factor out a shorter alias for them.

--- js/main.js
+++ js/main.js
@@ -5,16 +5,16 @@
     // Once the DOM is loaded, set things up.
     nurdz.contentLoaded (window, function ()
     {
+        var cv = nurdz.sneak.constants;
+
         // Set up the stage.
-        var stage = new nurdz.game.Stage (nurdz.sneak.constants.STAGE_WIDTH,
-                                          nurdz.sneak.constants.STAGE_HEIGHT,
-                                          'gameContent');
+        var stage = new nurdz.game.Stage (cv.STAGE_WIDTH, cv.STAGE_HEIGHT, 'gameContent');
 
         // Register all of our scenes.
-        stage.addScene (nurdz.sneak.constants.SCENE_TITLE, new nurdz.game.TitleScene ());
+        stage.addScene (cv.SCENE_TITLE, new nurdz.game.TitleScene ());
 
         // Switch to the title screen scene and run the game.
-        stage.switchToScene (nurdz.sneak.constants.SCENE_TITLE);
+        stage.switchToScene (cv.SCENE_TITLE);
         stage.run ();
     });
 } ());
