224fb42660ad5f67cecf199074ee9e8a1d7454e0
Terence Martin <poindexter@nurdz.com>
2015-10-27 22:07:14 -0700

    Level Swap
    Now there are a bunch of buttons and doors to make the sample level more
    interesting.
    
    In particular this shows how a button can revert itself back to an unpressed
    state if configured to do so, how a button can trigger a door, and how a
    button can trigger another pressed button in order to have it re-enable itself.

--- js/sneak/levels/level1.js
+++ js/sneak/levels/level1.js
@@ -26,11 +26,15 @@ nurdz.sneak.levels.level1 = new nurdz.game.LevelData ("level1", 25, 18, [
     // The location the player will start at.
     new nurdz.sneak.PlayerStartEntity (23, 11, {id: 'playerStart', visible: false}),
     new nurdz.sneak.LevelGoal (1, 5, {id: 'winGoal'}),
-    new nurdz.sneak.Door (4, 8, {open: false, closeTime: 20}),
-    new nurdz.sneak.Door (19, 11, {open: true, openTime: 3}),
+    new nurdz.sneak.Door (4, 8, {open: false, openTime: 14, id: "exitDoor"}),
+    new nurdz.sneak.Door (19, 11, {open: true, openTime: 3, id: "autoDoor1"}),
     new nurdz.sneak.Door (19, 15, {open: false, openTime: 2}),
-    new nurdz.sneak.Door (11, 4, {open: true, horizontal: true}),
-    new nurdz.sneak.Door (14, 4, {open: false, horizontal: true}),
-    new nurdz.sneak.Button(20, 13, {orientation: "left", pressed: true, cycleTime: 3})
+    new nurdz.sneak.Door (11, 10, {open: false, id: "door2"}),
+    new nurdz.sneak.Door (14, 4, {open: false, horizontal: true, id: "door1"}),
+    new nurdz.sneak.Button (20, 13, {orientation: "left", pressed: true, cycleTime: 3, trigger: "autoDoor1"}),
+    new nurdz.sneak.Button (15, 6, {orientation: "right", trigger: "door1"}),
+    new nurdz.sneak.Button (14, 1, {orientation: "top", trigger: "door2"}),
+    new nurdz.sneak.Button (10, 11, {orientation: "bottom", trigger: "exitDoor", pressed: true, id: "exitBtn"}),
+    new nurdz.sneak.Button (10, 15, {orientation: "top", trigger: "exitBtn"})
 ], nurdz.sneak.stdTiles);
 
